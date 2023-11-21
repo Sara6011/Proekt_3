83d8123b22bdf6ca75fc1a366f16d4c9ad53f2b2
Jankulovska <jankulovskasara60@gmail.com>
2023-11-21 13:27:03 +0100

    Add first java program

---
+++ src/Menuvacnica.java
@@ -0,0 +1,12 @@
+
+public class Menuvacnica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int d=65;
+        double e=61.6;
+        double x=d*e;
+        System.out.println(x);
+	}
+
+}
